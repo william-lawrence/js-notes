87f676b7544899d2b4e6534efd6d729c73d4c1e5
Will <literallythevoid@protonmail.com>
2021-11-07 09:48:32 -0500

    Add index and initial scripts

---
+++ javascript/values_and_variables.js
@@ -0,0 +1,41 @@
+/*
+  There are 5 basic primitive types along with two other less used type:
+
+  1. Number
+  2. String
+  3. Boolean
+  4. Null
+  5. Undefined
+  6. Symbol
+  7. BigInt
+*/
+
+/*
+  NUMBERS
+
+  js only has one type of number, where other languages have different types for different numbers.
+
+  It will not store an infinitely precise number. 
+
+  We have pretty standard math operators.
+*/
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function remainder(a, b) {
+  return a % b
+}
