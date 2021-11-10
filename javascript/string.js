b3e24f3e32369f7a92ed03d10082ee0d781d6ab6
Will <literallythevoid@protonmail.com>
2021-11-09 21:33:30 -0500

    Add more string methods

--- javascript/string.js
+++ javascript/string.js
@@ -13,3 +13,20 @@ function stringInterpolation(name) {
     return `Hello ${name}!`
   }
 }
+function textLength(text= "") {
+  return text.length
+}
+
+function printEachCharacter(text= "") {
+  for( let i = 0; i < text.length; i++) {
+    console.log(text[i])
+  }
+}
+
+function upperCaseExample(text="") {
+  return text.toUpperCase()
+}
+
+function lowerCaseExample(text="") {
+  return text.toLowerCase()
+}
