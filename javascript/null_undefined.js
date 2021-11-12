ff451b9f81bbb7421075133499e6b6cf6f8dda69
Will <literallythevoid@protonmail.com>
2021-11-12 07:26:16 -0500

    Add js examples for null and undefined

---
+++ javascript/null_undefined.js
@@ -0,0 +1,10 @@
+function undefinedExample() {
+  // when we have a variable that is not defined, we get undefined.
+  return undefinedVariable
+}
+
+function nullExample() {
+  // Null is a value that actually needs to be assigned in order to use it.
+  nullVariable = null
+  return nullVariable
+}
